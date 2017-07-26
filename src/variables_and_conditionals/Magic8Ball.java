8de3796e22dd2b0205a0e2e6f11bef14de7cd86b
dedethecoder <dedethecoder@gmail.com>
2017-07-25 18:22:47 -0700

    Add files via upload

--- src/variables_and_conditionals/Magic8Ball.java
+++ src/variables_and_conditionals/Magic8Ball.java
@@ -2,16 +2,22 @@ package variables_and_conditionals;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
     // 1. Make a main method that includes all the steps below….
-
+          public static void main(String[] args){
+        	  
+          
     // 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+    Random randy =  new Random();
+    int RandNum= randy.nextInt(4);
+    System.out.println(RandNum);
     // 3. Print out this variable
-
+             
     // 4. Get the user to enter a question for the 8 ball
-
+    String question=JOptionPane.showInputDialog("Insert Question");
     // 5. If the random number is 0
 
     // -- tell the user "Yes"
@@ -29,3 +35,4 @@ public class Magic8Ball {
     // -- write your own answer
 
 }
+}   
--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,70 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
-        // 2. Ask the user to enter a name. Store their answer in a variable.
-
+             String Hudaifa= "Turn into A duck";
+             String  Kyren="Invisibility";
+             String Deen="Flight";
+             String Matin="Time Travel";
+             String Dexter="Size Control";
+             String Dave="Super Strength";
+             String Matt="Breathe under water";
+             String Jah="Ice Control";
+             String Mike="Hiking 20 Miles In One Minute";
+             String  DatBoi="DatBoi uses Memes 4 life,On you its Effective";
+             // 2. Ask the user to enter a name. Store their answer in a variable.
+                 String answer=JOptionPane.showInputDialog("Enter the person whose power you want to see");
+                   
         // 3. Show the superpower in a pop-up, depending on the name entered. 
-
-    }
+              if(answer.equals("Matin")){
+            	  JOptionPane.showMessageDialog(null, Matin);
+              }
+              else if(answer.equals("Hudaifa")){
+            		  JOptionPane.showMessageDialog(null, Hudaifa); 
+            		 
+            	  }
+            	  
+              else if(answer.equals("Kyren")){
+            		  JOptionPane.showMessageDialog(null, Kyren); 
+            		 
+            	  }
+              else  if(answer.equals("Dave")){
+            		  JOptionPane.showMessageDialog(null, Dave); 
+            		 
+            	  }
+              else  if(answer.equals("Matt")){
+            		  JOptionPane.showMessageDialog(null, Matt); 
+            		 
+            	  }
+            	  
+              else  if(answer.equals("Dexter")){
+            		  JOptionPane.showMessageDialog(null, Dexter); 
+            		 
+            	  }
+              else  if(answer.equals("Deen")){
+            		  JOptionPane.showMessageDialog(null, Deen); 
+            		 
+            	  }
+            		
+              else  if(answer.equals("Jah")){
+            		  JOptionPane.showMessageDialog(null, Jah); 
+            		  
+            	  }
+              else  if(answer.equals("Mike")){
+            		  JOptionPane.showMessageDialog(null, Mike); 
+            	  }
+              else  if(answer.equals("DatBoi")){
+        		  JOptionPane.showMessageDialog(null,DatBoi ); 
+              }
+              
+              else{
+            	  JOptionPane.showMessageDialog(null, "No Power M8");
+              
+             }
+              
+        		  
+              
 }
+}
+
+
+
