c883169248e104fcc5885ff8d57411b37fa66172
dedethecoder <dedethecoder@gmail.com>
2017-10-03 18:24:45 -0700

    Add files via upload

--- src/variables_and_conditionals/ChangeCalculator.java
+++ src/variables_and_conditionals/ChangeCalculator.java
@@ -15,18 +15,19 @@ import javax.swing.JOptionPane;
 public class ChangeCalculator {
 
     public static void main(String[] args) {
-
+String Question=JOptionPane.showInputDialog("How Many Nickels Do You Have");
         // Ask the user how many nickels they have
-
+int Nickels=Integer.parseInt(Question);
         // Convert their answer to an int using Integer.parseInt()
-
+String QUESTION=JOptionPane.showInputDialog("How Many Dimes Do You Have");
         // Ask the user how many dimes they have, and convert their answer
-
+int Dimes=Integer.parseInt(QUESTION);
         // Ask the user how many quarters they have, and convert their answer
-
+String QuEsTiOn=JOptionPane.showInputDialog("How Many Quarters Do You Have");
         // Calculate how much money the user has and save it in a double variable 
-
+int Quarters=Integer.parseInt(QuEsTiOn);
         // Tell the user how much money they have
-
-    }
+double TotalMoney=Nickels*0.05+Dimes*0.10+Quarters*0.25;
+    System.out.println(TotalMoney);
+   JOptionPane.showMessageDialog(null,TotalMoney);     } 
 }
